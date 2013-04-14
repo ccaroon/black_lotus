3f181464090f004b6bf49f6c7da3f21dd180530f
Craig N. Caroon <craig-web@caroon.org>
2013-04-14 19:04:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackLotus::Application.config.secret_token = '317478b8a1c5c51f8141564d46989cc0de2828acebdc5d6dc9f68e3001a47e91e38039abb18877aaf6d12c3523395e81b6bc2f343d03437c52ccbaacb904514f'
