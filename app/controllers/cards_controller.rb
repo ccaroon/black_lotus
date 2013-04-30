d6f6155707587c278af5a54837bfaf5ae887e954
Craig N. Caroon <ccaroon@webassign.net>
2013-04-30 16:38:16 -0400

    * Added some basic layout/UI and pagination to Cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,7 @@ class CardsController < ApplicationController
   # GET /cards
   # GET /cards.json
   def index
-    @cards = Card.all
+    @cards = Card.order(:name).page(params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
