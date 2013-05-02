2a4b038427d5cc3f666a5092868324cc1279639f
Craig N. Caroon <craig-web@caroon.org>
2013-05-01 21:01:52 -0400

    * Started working on the card show screen

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,18 @@
 module CardsHelper
+  def rarity_to_html
+    css_class = ''
+    
+    case @card.rarity
+    when 'Common'      then css_class = ''
+    when 'Uncommon'    then css_class = 'badge'
+    when 'Rare'        then css_class = 'badge badge-warning'
+    when 'Mythic Rare' then css_class = 'badge badge-important'
+    end
+    
+    return css_class;
+  end
+  
+  def mana_cost_to_html
+  end
+  
 end
