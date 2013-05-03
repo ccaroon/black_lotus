1a934861dbf5a7c847606b75ed965e99f8f9975a
Craig N. Caroon <ccaroon@webassign.net>
2013-05-03 10:24:29 -0400

    * Added code to insert a placholder image in the Cards list   and on the Deck show pages if the image is missing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,15 @@ module ApplicationHelper
   
   def menu_item_active?(name)
     active = params[:controller] == name ? 'active' : ''
-    return active;
+    return active
+  end
+  
+  def card_image_path(card)
+    image_path = File.exist?("#{Rails.public_path}/card_images/#{card.image_name}")  ?
+      "/card_images/#{card.image_name}" :
+      "http://placehold.it/312x445/888888/000000&text=#{card.name}"
+
+    return image_path;
   end
   
 end
