1d20a2e8f21d91cfc58882fead594395f1263882
Craig N. Caroon <ccaroon@webassign.net>
2013-05-24 13:22:38 -0400

    * app/models/edition.rb     - removed some extra space * db/test.sqlite3     - migrated

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -3,7 +3,7 @@ class Edition < ActiveRecord::Base
 
   attr_accessible :card_count, :code_name, :name, :online_code, :release_date
   has_and_belongs_to_many :cards, :order => :name
-  ##############################################################################  
+  ##############################################################################
   def self.recent_editions
     one_yr_ago = Time.now - 1.year
     re = Edition
