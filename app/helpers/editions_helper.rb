d34e5c798d2be35c1228bd71d5967664e35852f2
Craig N. Caroon <ccaroon@webassign.net>
2013-05-30 16:33:15 -0400

    * #16 - Create and Edit Editions * Added bootstrap-datepicker plugin * Added some validation to Edition

--- app/helpers/editions_helper.rb
+++ app/helpers/editions_helper.rb
@@ -1,2 +1,5 @@
 module EditionsHelper
+  def edition_error?(attr)
+    display_error_if_error @edition, attr
+  end
 end
--- app/models/edition.rb
+++ app/models/edition.rb
@@ -1,6 +1,10 @@
 class Edition < ActiveRecord::Base
   paginates_per 10
 
+  validates_presence_of :name, :code_name, :release_date, :card_count
+  validates_numericality_of :card_count, 
+    :only_integer => true, :greater_than => 0
+
   attr_accessible :card_count, :code_name, :name, :online_code, :release_date
   has_and_belongs_to_many :cards, :order => :name
   ##############################################################################
