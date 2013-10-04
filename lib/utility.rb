b3fd2cb0c2657b6c57d931dea8dc487921005db7
Craig N. Caroon <ccaroon@webassign.net>
2013-10-04 14:42:27 -0400

    * Added ability to export cards in a format that mtgprice.com can import

--- lib/utility.rb
+++ lib/utility.rb
@@ -2,8 +2,17 @@ require 'csv'
 
 class Utility
   ##############################################################################
-  def self.export_cards
-    file_name = "#{Rails.root}/tmp/cards-#{Time.now.to_s.gsub(/[^0-9]/, '')}.csv"
+  def self.export_cards(format = :handbase)
+    case format
+    when :handbase
+      self.export_for_handbase()
+    when :mtgprice
+      self.export_for_mtgprice()
+    end
+  end
+  ##############################################################################
+  def self.export_for_handbase
+    file_name = "#{Rails.root}/tmp/cards-handbase-#{Time.now.to_s.gsub(/[^0-9]/, '')}.csv"
     CSV.open(file_name, "wb") do |csv|
       Card.all.each do |card|
         edition_str = card.editions.map {|e| e.name}.join('|')
@@ -15,6 +24,42 @@ class Utility
     end
 
     return (file_name)
+  end
+  ##############################################################################
+  def self.export_for_mtgprice_by_100
+    count = Card.count
+    num_files = count / 100
+    num_files += 1 if count % 100 != 0
+
+    1.upto(num_files) do |i|
+      file_name = "#{Rails.root}/tmp/cards-mtgprice-#{i}.csv"
+      File.open(file_name, "wb") do |file|
+        Card.page(i).per(100).each do |card|
+          edition = card.editions.pop
+          edition_str = edition.name =~ /^Magic 20/ ? 
+            edition.code_name                       : 
+            edition.name
+
+          file << "#{card.count}, #{card.name}, #{edition_str}\n"
+        end
+      end
+    end
+  end  
+  ##############################################################################
+  def self.export_for_mtgprice
+    file_name = "#{Rails.root}/tmp/cards-mtgprice-#{Time.now.to_s.gsub(/[^0-9]/, '')}.csv"
+    File.open(file_name, "wb") do |file|
+      Card.all.each do |card|
+        edition = card.editions.pop
+        edition_str = edition.name =~ /^Magic 20/ ? 
+          edition.code_name                       : 
+          edition.name
+
+        file << "#{card.count}, #{card.name}, #{edition_str}\n"
+      end
+    end
+
+    return (file_name)    
   end
   ##############################################################################
   def self.fetch_card_info(card)
